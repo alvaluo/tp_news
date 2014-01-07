efe305d440014c83e5ae8b21495f6c9de43c10b6
luo <luo@Alva-pc>
2014-01-08 01:21:28 +0800

    new update 20140108

--- tp_news/index.php
+++ tp_news/index.php
@@ -3,6 +3,20 @@
 define('APP_NAME','tp_news');
 define('APP_PATH','');
 
+$root=dirname(__FILE__);
+$root=str_replace('', '/', $root);
+define('__ROOT__',$root);
+/* define('LIB_PATH', __ROOT__.'/libs/');
+define('CONF_PATH', __ROOT__.'/config/');
+define('LANG_PATH', __ROOT__.'/lang/');
+define('TMPL_PATH', __ROOT__.'/templates/'); */
+define('RUNTIME_PATH',__ROOT__.'/Cache/');
+define('LOG_PATH', RUNTIME_PATH.'/log/');
+define('TEMP_PATH', RUNTIME_PATH.'/temp/');
+define('DATA_PATH',RUNTIME_PATH.'/data/');
+define('CACHE_PATH', RUNTIME_PATH.'/cache/');
+
+
 define('APP_DEBUG',TRUE);
 
 require './Vendor/ThinkPHP/ThinkPHP.php';
