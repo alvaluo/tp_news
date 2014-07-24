04e370aff21d85484c2f7910c2e7f36f791bdb95
alvaluo <alvaluo@wisers.com>
2014-07-24 17:48:50 +0800

    new update 20140724

--- tp_news/Lib/Model/EnterpriseModel.class.php
+++ tp_news/Lib/Model/EnterpriseModel.class.php
@@ -1,24 +1,51 @@
 <?php
-
-class  EnterpriseModel extends Model {
-
-	//模型命名
+class EnterpriseModel extends Model {
+	
+	// 模型命名
 	protected $trueTableName = 'enterprise';
 	
-	//数据模型
-	protected $fields = array(
-			'id', 'companyname', 'englishname', 'companynature ','companytype' , 'introduction','address','regname','companypic','logo','phone','fax','mobile','email','qq','createtime','type',
+	// 数据模型
+	protected $fields = array (
+			'id',
+			'companyname',
+			'englishname',
+			'companynature ',
+			'companytype',
+			'introduction',
+			'address',
+			'regname',
+			'companypic',
+			'logo',
+			'phone',
+			'fax',
+			'mobile',
+			'email',
+			'qq',
+			'web',
+			'contact',
+			'zipcode',
+			'createtime',
+			'type' 
 	);
 	
 	// 自动验证设置
-	protected $_validate	 =	 array(
-	);
+	protected $_validate = array ();
 	
 	// 自动填充设置
 	public $_auto = array (
-			array('id',null,self::MODEL_INSERT),
-			array('createtime','date',1,'function',array('Y-m-d H:i:s')),
+			array (
+					'id',
+					null,
+					self::MODEL_INSERT 
+			),
+			array (
+					'createtime',
+					'date',
+					1,
+					'function',
+					array (
+							'Y-m-d H:i:s' 
+					) 
+			) 
 	);
-	
-
- }
+}
